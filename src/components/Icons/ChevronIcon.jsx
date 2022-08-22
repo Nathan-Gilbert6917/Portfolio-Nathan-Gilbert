0215c0b2a5ca4f560ef71f352d45e9c28fe1e5b0
NathanGilbert6917 <weneedmoreminerals@gmail.com>
2022-08-21 19:38:08 -0700

    fix

---
+++ src/components/Icons/ChevronIcon.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const ChevronIcon = ({isUp = true}) => {
+  return (
+    <Fragment>
+      {isUp ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>}
+    </Fragment>
+  )
+}
+
+export default ChevronIcon;
